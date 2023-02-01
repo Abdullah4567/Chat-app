f8756023317fb39d684375fb3e61fc97eeb0ad9a
Abdullah4567 <gemstones321@gmail.com>
2023-02-01 08:36:35 +0500

    Error handling fixed.
    Express js does not handle errors in case of asynchronous program. So you have to wrap that in try catch block

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,44 +2,39 @@ const { validateSignUp } = require('../validators/validator')
 const User = require('../models/userModel');
 const generateToken = require('../config/generateToken');
 const hashPassword = require('../config/hashPassword');
-const registerUser = async (req, res) => {
-    const { error, value } = validateSignUp(req.body);
-    res.status(200).json({
-        success: true
-    })
-    // if (!error) {
-
-    //     // const err = new Error('Email Already Exists');
-    //     // err.status = 500;
-    //     // throw err;
-
-    //     const { name, email, password } = req.body;
-    //     const user = await User.findOne({ email });
-    //     if (!user) {
-    //         const pass = await hashPassword(password);
-    //         const newUser = await User.create({
-    //             email: email,
-    //             name: name,
-    //             password: pass
-    //         })
-    //         const token = await generateToken(newUser.id);
-    //         return res.status(201).json({
-    //             success: true,
-    //             token: token,
-    //             user: {
-    //                 id: newUser.id,
-    //                 name: newUser.name,
-    //                 email: newUser.email
-    //             }
-    //         })
-    //     }
-    //     else {
-
-    //         // return res.status(400).json({
-    //         //     success: false,
-    //         //     message: 'Email already exists'
-    //         // })
-    //     }
-    // }
+const registerUser = async (req, res, next) => {
+    try {
+        const { error, value } = validateSignUp(req.body);
+        if (!error) {
+            const { name, email, password } = req.body;
+            const user = await User.findOne({ email });
+            if (!user) {
+                const pass = await hashPassword(password);
+                const newUser = await User.create({
+                    email: email,
+                    name: name,
+                    password: pass
+                })
+                const token = await generateToken(newUser.id);
+                return res.status(201).json({
+                    success: true,
+                    token: token,
+                    user: {
+                        id: newUser.id,
+                        name: newUser.name,
+                        email: newUser.email
+                    }
+                })
+            }
+            else {
+                const err = new Error("Email already Taken");
+                err.status = 400;
+                throw err;
+            }
+        }
+    } catch (error) {
+        // console.log(error);
+        next(error);
+    }
 }
 module.exports = { registerUser }
